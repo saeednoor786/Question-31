43ba4b8a40a3cd7655df2a01a4097a0d53f3424b
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 23:42:43 +0500

    thirtyone assignment

---
+++ app.ts
@@ -0,0 +1,33 @@
+// Q:31 No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+
+let usernames: string[] = ["Saeed","Ahmed","Saudi","Nimra"];
+
+// check the list of users is not empty.
+if(usernames.length > 0){
+    // loop through the array and print each user
+    for(let user of usernames){
+        console.log("user:" + user);
+    }
+    
+} else {
+    console.log("We need to find some users!"); 
+}
+
+// Removing all usernames ensures the message "We need to find some users!" is printed.
+
+let nullNames: string [] = [];
+
+if(nullNames.length > 0){
+    // loop through the array and print each user
+    for(let user of nullNames){
+        console.log("user:" + user);
+    }
+    
+} else {
+    console.log("We need to find some users!"); 
+}
+
+
